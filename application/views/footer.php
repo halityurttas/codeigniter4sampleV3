3b88ec4440b32fd764e2d6b1689771089db34c04
= <=>
2016-04-06 08:01:51 +0000

    ders 13 ara güncelleme

---
+++ application/views/footer.php
@@ -0,0 +1,4 @@
+    <script type="text/javascript" src="<?php echo base_url(); ?>/contents/js/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>/contents/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CI V3 Dersler</title>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/contents/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/contents/css/bootstrap-theme.min.css" type="text/css" />
+    </head>
+    <body>
+        
